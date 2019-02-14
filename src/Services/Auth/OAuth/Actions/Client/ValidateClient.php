48ace280b55e6e02d6ba463d14f5f2628fff2dc6
Tangomajom <kutyafika>
2019-02-14 14:52:53 +0100

    Grant type check for client validation has been added.

--- src/Services/Auth/OAuth/Actions/Client/ValidateClient.php
+++ src/Services/Auth/OAuth/Actions/Client/ValidateClient.php
@@ -26,16 +26,19 @@ class ValidateClient extends APIRepositoryAction
         /** @var Client $client */
         $client = $args["client"]; //Todo move to constant;
         $validateSecret = $args["validate_secret"] ?? true; //Todo move to constant
+        $grantType = $args["grant_type"]; //Todo move to constant
 
         $storedData = $this->database->get("OAuth_Clients",
-            [ "Id", "Secret"],
-            [
-                "Id" => $client->getIdentifier()
-            ]
+            ["Secret"],
+            ["Id" => $client->getIdentifier()]
         );
 
         if(!$storedData || empty($storedData) || is_null($storedData)){
             throw new UnauthorizedException("Client not registered");
+        }
+
+        if(!$client->hasGrantType($grantType)){
+            throw new UnauthorizedException($grantType." grant type not registered for Client ".$client->getName().".");
         }
 
         if($validateSecret && !password_verify($client->getSecret(),$storedData["Secret"])){
