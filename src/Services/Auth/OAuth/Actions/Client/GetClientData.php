c3e78710001726b73cacb6596de1213cc177bdf2
Tangomajom <kutyafika>
2019-02-14 14:36:48 +0100

    Grant type data has been added to the GetClientData action.

--- src/Services/Auth/OAuth/Actions/Client/GetClientData.php
+++ src/Services/Auth/OAuth/Actions/Client/GetClientData.php
@@ -10,8 +10,8 @@ namespace MedevAuth\Services\Auth\OAuth\Actions\Client;
 
 
 use MedevAuth\Services\Auth\OAuth\Entity\Client;
-use MedevAuth\Services\Auth\OAuth\Repository\Exception\RepositoryException;
 use MedevSlim\Core\Action\Repository\APIRepositoryAction;
+use MedevSlim\Core\Service\Exceptions\UnauthorizedException;
 
 class GetClientData extends APIRepositoryAction
 {
@@ -19,24 +19,30 @@ class GetClientData extends APIRepositoryAction
     /**
      * @param $args
      * @return Client
-     * @throws RepositoryException
+     * @throws UnauthorizedException
      */
     public function handleRequest($args)
     {
-        $clientIdentifier = $args["client_id"]; //Todo move to constant
+        $clientId = $args["client_id"]; //Todo move to constant
 
-        $storedData = $this->db->get("OAuth_Clients",
+        $storedData = $this->database->get("OAuth_Clients",
             [
                 "Id","Name","Secret","RedirectURI","Status"
             ],
             [
-                "Id" => $clientIdentifier
+                "Id" => $clientId
             ]
         );
 
-        //Todo test it briefly
+        $grantTypes = $this->database->select("OAuth_ClientGrantTypes", //From Table
+            ["[>]OAuth_GrantTypes" => ["GrantId" => "Id"]],     //Table Join section
+            ["GrantName"],                                      //Select column list
+            ["ClientId" => $clientId]                           //Where condition
+        );
+
+
         if(empty($storedData) || is_null($storedData)){
-            throw new RepositoryException("Client ".$clientIdentifier." not existing in the database.");
+            throw new UnauthorizedException("Client ".$clientId." not existing in the database.");
         }
 
         $clientEntity = new Client();
@@ -44,6 +50,7 @@ class GetClientData extends APIRepositoryAction
         $clientEntity->setName($storedData["Name"]);
         $clientEntity->setRedirectUri($storedData["RedirectURI"]);
         $clientEntity->setSecret($storedData["Secret"]);
+        $clientEntity->setGrantTypes($grantTypes);
 
         return $clientEntity;
     }
