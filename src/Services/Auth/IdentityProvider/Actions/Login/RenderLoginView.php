f05da4d6fdbf24d040dc63fe3d07759f30d5723b
Tangomajom <kutyafika>
2019-02-19 18:52:36 +0100

    Code cleanup.

--- src/Services/Auth/IdentityProvider/Actions/Login/RenderLoginView.php
+++ src/Services/Auth/IdentityProvider/Actions/Login/RenderLoginView.php
@@ -12,8 +12,6 @@ namespace MedevAuth\Services\Auth\IdentityProvider\Actions\Login;
 use MedevSlim\Core\Action\Servlet\Twig\APITwigServlet;
 use Slim\Http\Request;
 use Slim\Http\Response;
-use Slim\Views\Twig;
-use Twig\Loader\FilesystemLoader;
 
 class RenderLoginView extends APITwigServlet
 {
@@ -23,19 +21,9 @@ class RenderLoginView extends APITwigServlet
      * @param Response $response
      * @param $args
      * @return \Psr\Http\Message\ResponseInterface
-     * @throws \Twig_Error_Loader
      */
     public function handleRequest(Request $request, Response $response, $args)
     {
-        /** @var Twig $view */
-        $view = $this->service->getContainer()->get("view");
-        /** @var FilesystemLoader $viewLoader */
-        $viewLoader = $this->service->getContainer()->get("view_loader");
-        $viewLoader->addPath(__DIR__."/View", $this->service->getServiceName());
-
-
-        $this->info("Megvan a view");
-
         return $this->render($response,"LoginScreen.twig",[]);
     }
 }
