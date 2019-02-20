5f72f74f0a024ceb2bd4728f9730df61d429b86d
Tangomajom <kutyafika>
2019-02-20 13:59:52 +0100

    Grant type related common methods are collected in an abstract class.

---
+++ src/Services/Auth/OAuth/Actions/GrantType/GrantAccess.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: OargaTamas
+ * Date: 2019. 02. 20.
+ * Time: 11:22
+ */
+
+namespace MedevAuth\Services\Auth\OAuth\Actions\GrantType;
+
+
+use MedevAuth\Services\Auth\OAuth\Entity\Client;
+use MedevAuth\Services\Auth\OAuth\Entity\Token\OAuthToken;
+use MedevAuth\Services\Auth\OAuth\Entity\User;
+use MedevSlim\Core\Action\Servlet\APIServlet;
+use MedevSlim\Core\Service\Exceptions\UnauthorizedException;
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+/**
+ * Class GrantAccess
+ * @inheritdoc
+ * @package MedevAuth\Services\Auth\OAuth\Actions\GrantType
+ */
+abstract class GrantAccess extends APIServlet
+{
+    const ACCESS_TOKEN = "access_token";
+    const ACCESS_TOKEN_TYPE = "token_type";
+    const EXPIRES_IN = "expires_in";
+    const REFRESH_TOKEN = "refresh_token";
+
+    /**
+     * @var User
+     */
+    protected $user;
+    /**
+     * @var Client
+     */
+    protected $client;
+    /**
+     * @var string[]
+     */
+    protected $scopes;
+
+    /**
+     * @param Request $request
+     * @param Response $response
+     * @param $args
+     * @return Response
+     * @throws \Exception
+     */
+    public function handleRequest(Request $request, Response $response, $args)
+    {
+        $this->info("Validating token request");
+        $this->validateAccessRequest($request, $args);
+
+        $this->info("Access granted. Generating tokens based on the grant flow.");
+        $accessToken = $this->generateAccessToken();
+        $refreshToken = $this->generateRefreshToken();
+
+        $data = [
+            self::ACCESS_TOKEN => $accessToken->finalizeToken(),
+            self::ACCESS_TOKEN_TYPE => "bearer",
+            self::EXPIRES_IN => $accessToken->getExpiration(),
+        ];
+
+        if ($refreshToken != null) {
+            $data[self::REFRESH_TOKEN] = $refreshToken->finalizeToken();
+        }
+
+        return $response->withJson($data, 200);
+    }
+
+
+    /**
+     * @param Request $request
+     * @param array $args
+     * @return void
+     * @throws UnauthorizedException
+     */
+    public abstract function validateAccessRequest(Request $request, $args = []);
+
+    /**
+     * @throws \Exception
+     * @return OAuthToken
+     */
+    public abstract function generateAccessToken();
+
+    /**
+     * @return OAuthToken|null
+     */
+    public function generateRefreshToken()
+    {
+        //By default no refresh token is provided
+        //Override and return valid token if needed in the specified grant type class
+        $this->info("Skipping refresh token generation.");
+        return null;
+    }
+}
