b4a62319708278cc410f9a5c89250be08b20ea99
Tangomajom <kutyafika>
2019-02-25 19:30:46 +0100

    Implicit grant type token and auth endpoints has been added. Logic is the same like auth code grant but we not using client secret.

---
+++ src/Services/Auth/OAuth/Actions/GrantType/Flows/Implicit/AuthorizeRequest.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: OargaTamas
+ * Date: 2019. 02. 25.
+ * Time: 9:35
+ */
+
+namespace MedevAuth\Services\Auth\OAuth\Actions\GrantType\Flows\Implicit;
+
+
+use MedevAuth\Services\Auth\OAuth\Actions\Client\ValidateClient;
+use MedevAuth\Services\Auth\OAuth\Actions\GrantType\Authorization\Authorization;
+use MedevAuth\Services\Auth\OAuth\Actions\Token\JWT\JWS\AccessToken\GenerateAccessToken;
+use MedevAuth\Services\Auth\OAuth\Entity\Token\OAuthToken;
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+class AuthorizeRequest extends Authorization
+{
+
+    private $scopes;
+
+    public function isLoginRequired(Request $request, $args)
+    {
+        if (isset($_SESSION["user"])) {
+            $this->user = $_SESSION["user"];
+            return false;
+        }
+
+        return true;
+    }
+
+    public function verifyClient(Request $request, $args)
+    {
+        $this->scopes = $request->getParam("scopes");
+
+        $validateClient = new ValidateClient($this->service);
+        $validateClient->handleRequest([
+            "client" => $this->client,
+            "validate_secret" => false,
+        ]);
+    }
+
+    public function buildSuccessResponse(Response $response, $args)
+    {
+        $this->info("Generating access token.");
+        $tokenInfo = [
+            OAuthToken::USER => $this->user,
+            OAuthToken::CLIENT => $this->client,
+            OAuthToken::SCOPES => $this->scopes
+        ];
+        $action = new GenerateAccessToken($this->service);
+        $accessToken = $action->handleRequest($tokenInfo);
+        return  $accessToken;
+    }
+}
---
+++ src/Services/Auth/OAuth/Actions/GrantType/Flows/Implicit/RequestAccessToken.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: OargaTamas
+ * Date: 2019. 02. 24.
+ * Time: 19:42
+ */
+
+namespace MedevAuth\Services\Auth\OAuth\Actions\GrantType\Implicit;
+
+
+use MedevAuth\Services\Auth\OAuth\Actions\GrantType\GrantAccess;
+use MedevAuth\Services\Auth\OAuth\Entity\Token\OAuthToken;
+use MedevSlim\Core\Service\Exceptions\UnauthorizedException;
+use Slim\Http\Request;
+
+class RequestAccessToken extends GrantAccess
+{
+
+    /**
+     * @param Request $request
+     * @param array $args
+     * @return void
+     * @throws UnauthorizedException
+     */
+    public function validateAccessRequest(Request $request, $args = [])
+    {
+        // TODO: Implement validateAccessRequest() method.
+    }
+
+    /**
+     * @throws \Exception
+     * @return OAuthToken
+     */
+    public function generateAccessToken()
+    {
+        // TODO: Implement generateAccessToken() method.
+    }
+}
