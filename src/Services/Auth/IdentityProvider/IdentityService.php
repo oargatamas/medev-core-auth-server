c1f447b30aa187dcbc5a3639e3cdb66932c6d616
Tangomajom <kutyafika>
2019-02-25 14:47:33 +0100

    RequestValidators has been added for the idp routes.

--- src/Services/Auth/IdentityProvider/IdentityService.php
+++ src/Services/Auth/IdentityProvider/IdentityService.php
@@ -17,6 +17,7 @@ use MedevAuth\Services\Auth\IdentityProvider\Actions\PasswordForgot\ForgotPasswo
 use MedevAuth\Services\Auth\IdentityProvider\Actions\PasswordForgot\RenderForgotPasswordView;
 use MedevAuth\Services\Auth\IdentityProvider\Actions\Register\RegisterServlet;
 use MedevAuth\Services\Auth\IdentityProvider\Actions\Register\RenderRegisterView;
+use MedevSlim\Core\Action\Middleware\RequestValidator;
 use MedevSlim\Core\Service\View\TwigAPIService;
 use Slim\App;
 
@@ -37,17 +38,36 @@ class IdentityService extends TwigAPIService
      */
     protected function registerRoutes(App $app)
     {
-        $app->get("/login",new RenderLoginView($this))->setName($this->getServiceName());
-        $app->post("/login",new LoginServlet($this))->setName($this->getServiceName());
+        $app->get("/", new RenderLoginView($this))
+            ->setName($this->getServiceName());
 
-        $app->get("/logout", new RenderLogoutView($this))->setName($this->getServiceName());
-        $app->post("/logout", new LogoutServlet($this))->setName($this->getServiceName());
+        $app->get("/login",new RenderLoginView($this))
+            ->setName($this->getServiceName());
 
-        $app->get("/register", new RenderRegisterView($this))->setName($this->getServiceName());
-        $app->post("/register", new RegisterServlet($this))->setName($this->getServiceName());
+        $app->post("/login",new LoginServlet($this))
+            ->add(new RequestValidator(LoginServlet::getParams()))
+            ->setName($this->getServiceName());
 
-        $app->get("/forgot", new RenderForgotPasswordView($this))->setName($this->getServiceName());
-        $app->post("/forgot", new ForgotPasswordServlet($this))->setName($this->getServiceName());
+        $app->get("/logout", new RenderLogoutView($this))
+            ->setName($this->getServiceName());
+
+        $app->post("/logout", new LogoutServlet($this))
+            ->add(new RequestValidator(LogoutServlet::getParams()))
+            ->setName($this->getServiceName());
+
+        $app->get("/register", new RenderRegisterView($this))
+            ->setName($this->getServiceName());
+
+        $app->post("/register", new RegisterServlet($this))
+            ->add(new RequestValidator(RegisterServlet::getParams()))
+            ->setName($this->getServiceName());
+
+        $app->get("/forgot", new RenderForgotPasswordView($this))
+            ->setName($this->getServiceName());
+
+        $app->post("/forgot", new ForgotPasswordServlet($this))
+            ->add(new RequestValidator(ForgotPasswordServlet::getParams()))
+            ->setName($this->getServiceName());
     }
 
 
