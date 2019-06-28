7ae5fe4adeaa879c87f2e3a2e29c13c1c4e0cf40
Tangomajom <kutyafika>
2019-06-28 10:12:24 +0200

    ChangePassword business logic has been added and integrated with the Request object.

---
+++ src/Services/Auth/IdentityProvider/Actions/PasswordForgot/ChangePassword.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: OargaTamas
+ * Date: 2019. 06. 28.
+ * Time: 9:22
+ */
+
+namespace MedevAuth\Services\Auth\IdentityProvider\Actions\PasswordForgot;
+
+
+use MedevAuth\Services\Auth\OAuth\Entity;
+use MedevAuth\Services\Auth\OAuth\Entity\Persistables;
+use MedevSlim\Core\Action\Repository\APIRepositoryAction;
+use MedevSlim\Core\Service\Exceptions\APIException;
+
+class ChangePassword extends APIRepositoryAction
+{
+
+    /**
+     * @param $args
+     * @throws APIException
+     */
+    public function handleRequest($args = [])
+    {
+        /** @var Entity\User $user */
+        $user = $args["user"];
+        $newPassword = $args["newPassword"];
+
+        $passwordHash = password_hash($newPassword,PASSWORD_BCRYPT,["cost" => 12]);
+
+        $result = $this->database->update(Persistables\User::getTableName(),
+            [
+                "Password" => $passwordHash
+            ],
+            [
+                "Id" => $user->getIdentifier()
+            ]
+        );
+
+        $error = $this->database->error();
+        if(isset($error[2]) || $result->rowCount() <= 0){
+            throw new APIException("Password can not be updated in database: ".implode(" - ",$error));
+        }
+    }
+}
