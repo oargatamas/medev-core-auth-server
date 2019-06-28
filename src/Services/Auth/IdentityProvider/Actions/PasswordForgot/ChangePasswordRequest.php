cd4e429425d928e45c9aea15993f1498e4294617
Tangomajom <kutyafika>
2019-06-28 09:46:59 +0200

    ChangePasswordRequest has been added to the repository.

---
+++ src/Services/Auth/IdentityProvider/Actions/PasswordForgot/ChangePasswordRequest.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: OargaTamas
+ * Date: 2019. 06. 28.
+ * Time: 9:03
+ */
+
+namespace MedevAuth\Services\Auth\IdentityProvider\Actions\PasswordForgot;
+
+
+use MedevAuth\Services\Auth\OAuth\Entity\Token\JWT\Signed\OAuthJWS;
+use MedevAuth\Services\Auth\OAuth\Entity\User;
+use MedevSlim\Core\Action\Servlet\APIServlet;
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+class ChangePasswordRequest extends APIServlet
+{
+
+    /**
+     * @param Request $request
+     * @param Response $response
+     * @param $args
+     * @return Response
+     * @throws \Exception
+     */
+    public function handleRequest(Request $request, Response $response, $args)
+    {
+        /** @var OAuthJWS $accessToken */
+        $accessToken = $args["token"];
+        /** @var User $user */
+        $user = $accessToken->getUser();
+
+        $validatePws = new ValidatePasswords($this->service);
+        $validateParams = $request->getParams(ChangePasswordRequest::getParams());
+        $validateParams["user"] = $user;
+        $validatePws->handleRequest([$validateParams]);
+
+
+        $changePw = new ChangePassword($this->service);
+        $changeParams = $request->getParams(ChangePasswordRequest::getParams());
+        $changeParams["user"] = $user;
+        $changePw->handleRequest([$changeParams]);
+
+
+        $data = [
+            "changed" => true
+        ];
+
+        return $response
+            ->withStatus(201)
+            ->withJson($data);
+    }
+
+    static function getParams()
+    {
+        return [
+            "oldPassword",
+            "newPassword",
+            "newPasswordAgain"
+        ];
+    }
+
+
+    static function getScopes()
+    {
+        return [
+            "update:userpw"
+        ];
+    }
+
+
+}
