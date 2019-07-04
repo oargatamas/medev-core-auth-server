b986c2c28f7942339f8afefccbda6ca2608552a8
Tangomajom <kutyafika>
2019-07-04 13:49:59 +0200

    Login code endpoint added to IDP service.

--- src/Services/Auth/IdentityProvider/IdentityService.php
+++ src/Services/Auth/IdentityProvider/IdentityService.php
@@ -11,6 +11,7 @@ namespace MedevAuth\Services\Auth\IdentityProvider;
 
 use MedevAuth\Services\Auth\IdentityProvider\Actions\Login\Login;
 use MedevAuth\Services\Auth\IdentityProvider\Actions\Login\RenderLoginView;
+use MedevAuth\Services\Auth\IdentityProvider\Actions\Login\Type\AuthCode\RequestLoginCode;
 use MedevAuth\Services\Auth\IdentityProvider\Actions\Logout\Logout;
 use MedevAuth\Services\Auth\IdentityProvider\Actions\Logout\RenderLogoutView;
 use MedevAuth\Services\Auth\IdentityProvider\Actions\PasswordRecovery\ChangePasswordRequest;
@@ -28,6 +29,7 @@ class IdentityService extends TwigAPIService
     const ID_TOKEN_REQUEST_KEY = "X-MEDEV-ID-TOKEN";
 
     const ROUTE_LOGIN = "login";
+    const ROUTE_LOGIN_CODE = "logincode";
     const ROUTE_LOGOUT = "logout";
     const ROUTE_REGISTER = "register";
     const ROUTE_FORGOT_PASSWORD = "forgot";
@@ -67,6 +69,11 @@ class IdentityService extends TwigAPIService
 
         $app->post("/login",new Login($this))
             ->add(new RequestValidator(Login::getParams()))
+            ->setArgument(APIService::SERVICE_ID,$this->getServiceName())
+            ->setName(self::ROUTE_LOGIN.".post");
+
+        $app->post("/login/code",new RequestLoginCode($this))
+            ->add(new RequestValidator(RequestLoginCode::getParams()))
             ->setArgument(APIService::SERVICE_ID,$this->getServiceName())
             ->setName(self::ROUTE_LOGIN.".post");
 
